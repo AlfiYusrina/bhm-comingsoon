095dbdaeb7edd51c38c682ea6f51c64c9ad368ac
Alfi Yusrina <alfi.yusrina@hotmail.com>
2020-01-17 18:51:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './bhm-logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faInstagram, faFacebookF} from '@fortawesome/free-brands-svg-icons'
+library.add(faInstagram, faFacebookF);
+dom.watch();
+class App extends React.Component{
+  render() {
+    return(
+      <div className="App">
+        <header className="header">
+          <div className="wrapper">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div >
+            <h1>
+              Coming soon...
+            </h1>
+            <div>
+              <a className="social-item fb" title="facebook" href="https://www.facebook.com/blackhistorymonthbelgium/" target="_blank"><i className="fab fa-facebook-f"></i></a>
+              <a className="social-item ig" title="instagram" href="https://www.instagram.com/bhmbelgium/"  target="_blank"><i className="fab fa-instagram"/></a>
+            </div>
+          </div>
+          </div>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
